4ad2f6419ab459990c1b4946b7b7152288a8d482
harbie34 <harbie34@d762efa6-ad36-0410-95e0-2b603ca9a8e5>
2010-02-25 00:15:47 +0000

    環境毎のechoコマンドの違いを吸収するよう修正。(｀･ω･´)
    git-svn-id: https://beckorz.googlecode.com/svn/trunk@63 d762efa6-ad36-0410-95e0-2b603ca9a8e5

--- shell_script/showpalette.sh
+++ shell_script/showpalette.sh
@@ -1,57 +1,67 @@
 #!/bin/sh
 # ANSIカラーチェックスクリプト
 
+# echoコマンドの違いを吸収する関数定義
+case "`echo -e`" in
+-e)
+	ECHO() { echo "$@"; }
+	;;
+*)
+	ECHO() { echo -e "$@"; }
+	;;
+esac
+
 # 属性名称
 get_attr_name() {
 	case "$1" in
-	0) echo -n "標準";;
-	1) echo -n "強調";;
-	2) echo -n "垂線";;
-	4) echo -n "下線";;
-	5) echo -n "点滅";;
-	7) echo -n "反転";;
-	8) echo -n "非表示";;
+	0) ECHO "標準\c";;
+	1) ECHO "強調\c";;
+	2) ECHO "垂線\c";;
+	4) ECHO "下線\c";;
+	5) ECHO "点滅\c";;
+	7) ECHO "反転\c";;
+	8) ECHO "非表示\c";;
 	esac
 }
 
 # 一般的なカラーインデックス名称
 get_color_name() {
 	case "$1" in
-	0) echo -n "Black   ";;
-	1) echo -n "Red     ";;
-	2) echo -n "Green   ";;
-	3) echo -n "Yellow  ";;
-	4) echo -n "Blue    ";;
-	5) echo -n "Magenta ";;
-	6) echo -n "Cyan    ";;
-	7) echo -n "White   ";;
+	0) ECHO "Black   \c";;
+	1) ECHO "Red     \c";;
+	2) ECHO "Green   \c";;
+	3) ECHO "Yellow  \c";;
+	4) ECHO "Blue    \c";;
+	5) ECHO "Magenta \c";;
+	6) ECHO "Cyan    \c";;
+	7) ECHO "White   \c";;
 	esac
 }
 
 # 行セパレータ出力
 print_separator() {
 	for LOOP in `seq 0 8`; do
-		echo -n "+--------"
+		ECHO "+--------\c"
 	done
-	echo
+	ECHO
 }
 
 for ATTR in 0 1 4 5; do
 	# ヘッダ出力
-	echo `get_attr_name $ATTR`
+	ECHO `get_attr_name $ATTR`
 	for FGCOL in `seq 0 7`; do
-		echo -n "|"
+		ECHO "|\c"
 		get_color_name ${FGCOL}
 	done
-	echo "|fg/bg"
+	ECHO "|fg/bg"
 	print_separator
 
 	# 表生成
 	for BGCOL in `seq 0 7`; do
 		for FGCOL in `seq 0 7`; do
-			echo -en "|\033[4${BGCOL}m\033[${ATTR};3${FGCOL}m Sample \033[m"
+			ECHO "|\033[4${BGCOL}m\033[${ATTR};3${FGCOL}m Sample \033[m\c"
 		done
-		echo "|"`get_color_name $BGCOL`
+		ECHO "|"`get_color_name $BGCOL`
 		print_separator
 	done
 done
