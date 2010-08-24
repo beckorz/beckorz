9f2dce45bb86590117bcfbf90a864af44415f1a4
harbie34 <harbie34@d762efa6-ad36-0410-95e0-2b603ca9a8e5>
2010-08-24 08:57:11 +0000

    ・Yahoo Pipes版スクリプト追加(｀･ω･´) ・Google版はファイル名変更しますた。
    git-svn-id: https://beckorz.googlecode.com/svn/trunk@92 d762efa6-ad36-0410-95e0-2b603ca9a8e5

---
+++ Charu3ExecEx/scripts/ja2en_y.js
@@ -0,0 +1,34 @@
+/////////////////////////////////////////////////////////////////////////
+//	Yahoo Pipes翻訳APIを使用した簡易日英翻訳スクリプト
+//		2010/08/24　うっちー
+/////////////////////////////////////////////////////////////////////////
+var URL = "http://pipes.yahoo.com/poolmmjp/je_translation_api?_render=json&text=";
+
+var httpObj = createXMLHttp();
+if (httpObj) {
+	var query = WScript.StdIn.ReadAll();
+	httpObj.open("GET", URL + encodeURIComponent(query), false);
+	httpObj.send(null);
+	if (httpObj.status == 200) {
+		var json = eval("(" + httpObj.responseText + ")");
+		WScript.StdOut.Write(json.value.items[0].description);
+	} else {
+		WScript.StdOut.Write("通信エラー");
+	}
+}
+
+function createXMLHttp() {
+	var ret = null;
+	try {
+		ret = new XMLHttpRequest();
+	} catch (e) {
+		try {
+			ret = new ActiveXObject("Msxml2.XMLHTTP");
+		} catch (e) {
+			try {
+				ret = new ActiveXObject("Microsoft.XMLHTTP");
+			} catch (e) { }
+		}
+	}
+	return ret;
+}
