51db74d67b09510147b13b0b6d766afac25e3d4b
harbie34 <harbie34@d762efa6-ad36-0410-95e0-2b603ca9a8e5>
2010-09-14 01:40:27 +0000

    英日・日英翻訳を言語自動判定付きにしてみますた。(｀･ω･´) 寿司とか富士山は中国語らしいですょッ！(´･ω･｀)
    git-svn-id: https://beckorz.googlecode.com/svn/trunk@99 d762efa6-ad36-0410-95e0-2b603ca9a8e5

---
+++ Charu3ExecEx/scripts/trans_g.js
@@ -0,0 +1,62 @@
+/////////////////////////////////////////////////////////////////////////
+//	Google翻訳APIを使用した簡易英日・日英翻訳スクリプト
+//		2010/09/14　うっちー
+/////////////////////////////////////////////////////////////////////////
+var DETECTURL = "http://ajax.googleapis.com/ajax/services/language/detect?v=1.0&q=";
+var TRANSURL =  "http://ajax.googleapis.com/ajax/services/language/translate?v=1.0&q=";
+
+var httpObj = createXMLHttp();
+if (httpObj) {
+	var QUERY = WScript.StdIn.ReadAll();
+	httpRequest(httpObj, DETECTURL + encodeURIComponent(QUERY), detectSuccess, requestError);
+}
+
+function createXMLHttp() {
+	var ret = null;
+	try {
+		ret = new XMLHttpRequest();
+	} catch (e) {
+		try {
+			ret = new ActiveXObject("Msxml2.XMLHTTP");
+		} catch (e) {
+			try {
+				ret = new ActiveXObject("Microsoft.XMLHTTP");
+			} catch (e) { }
+		}
+	}
+	return ret;
+}
+
+function httpRequest(obj, url, onsuccess, onerror) {
+	obj.open("GET", url, false);
+	obj.send(null);
+	if (obj.status == 200) {
+		onsuccess(obj);
+	} else {
+		onerror(obj);
+	}
+}
+
+function detectSuccess(obj) {
+	var json = eval("(" + obj.responseText + ")");
+	if (json.responseStatus == 200) {
+		// 漢字が中国語判定されがちなのでen以外はjaと見なす
+		var langpair = (json.responseData.language == "en") ? "en%7Cja" : "ja%7Cen";
+		httpRequest(obj, TRANSURL + encodeURIComponent(QUERY) + "&langpair=" + langpair, transSuccess, requestError);
+	} else {
+		WScript.StdOut.Write(json.responseDetails);
+	}
+}
+
+function transSuccess(obj) {
+	var json = eval("(" + obj.responseText + ")");
+	if (json.responseStatus == 200) {
+		WScript.StdOut.Write(json.responseData.translatedText);
+	} else {
+		WScript.StdOut.Write(json.responseDetails);
+	}
+}
+
+function requestError(obj) {
+	WScript.StdOut.Write("通信エラー");
+}
