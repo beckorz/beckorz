1ce3961a5beb3982dadcd3484a4b38b496e65060
harbie34 <harbie34@d762efa6-ad36-0410-95e0-2b603ca9a8e5>
2011-07-13 06:11:53 +0000

    bashサンプル若干書き換え＆追記。
    git-svn-id: https://beckorz.googlecode.com/svn/trunk@117 d762efa6-ad36-0410-95e0-2b603ca9a8e5

--- shell_script/bash_example.sh
+++ shell_script/bash_example.sh
@@ -2,143 +2,201 @@
 #	bashサンプル集
 
 # 変数が未定義のとき、デフォルト値を返す
-echo "\$V1=${V1-default}"
-V2=
-echo "\$V2=${V2-default}"
-# $V1=default
-# $V2=
+echo "\${V01-default}=${V01-default}"
+V02=
+echo "\${V02-default}=${V02-default}"
+# ${V01-default}=default
+# ${V02-default}=
 
 # 変数が未定義か空文字列のとき、デフォルト値を返す
-echo "\$V3=${V3:-default}"
-V4=
-echo "\$V4=${V4:-default}"	
-# $V3=default
-# $V4=default
+echo "\${V03:-default}=${V03:-default}"
+V04=
+echo "\${V04:-default}=${V04:-default}"
+# ${V03:-default}=default
+# ${V04:-default}=default
 
 # 変数が未定義のとき、デフォルト値をセットして返す
-echo "\$V5=${V5=default}"	
-V6=
-echo "\$V6=${V6=default}"	
-# $V5=default
-# $V6=
-echo "\$V5=${V5}" 
-echo "\$V6=${V6}" 
-# $V5=default
-# $V6=
+echo "\${V05=default}=${V05=default}"
+V06=
+echo "\${V06=default}=${V06=default}"
+echo "\$V05=${V05}"
+echo "\$V06=${V06}"
+# ${V05=default}=default
+# ${V06=default}=
+# $V05=default
+# $V06=
 
 # 変数が未定義か空文字列のとき、デフォルト値をセットして返す
-echo "\$V7=${V7:=default}"	
-V8=
-echo "\$V8=${V8:=default}"	
-# $V7=default
-# $V8=default
-echo "\$V7=${V7}"	
-echo "\$V8=${V8}"	
-# $V7=default
-# $V8=default
+echo "\${V07:=default}=${V07:=default}"
+V08=
+echo "\${V08:=default}=${V08:=default}"
+echo "\$V07=${V07}"
+echo "\$V08=${V08}"
+# ${V07:=default}=default
+# ${V08:=default}=default
+# $V07=default
+# $V08=default
+
+# 変数に何らかの値(空文字列含む)が設定されているとき、指定値を返す
+echo "\${V09+value}=${V09+value}"
+V10=
+echo "\${V10+value}=${V10+value}"
+V11=sample
+echo "\${V11+value}=${V11+value}"
+# ${V09+value}=
+# ${V10+value}=value
+# ${V11+value}=value
+
+# 変数に何らかの値(空文字列含まない)が設定されているとき、指定値を返す
+echo "\${V12:+value}=${V12:+value}"
+V13=
+echo "\${V13:+value}=${V13:+value}"
+V14=sample
+echo "\${V14:+value}=${V14:+value}"
+# ${V12:+value}=
+# ${V13:+value}=
+# ${V14:+value}=value
 
 # 変数にセットされている文字数を返す
-V9=123456789
-echo "count=${#V9}"	
-# count=9
+V15=123456789
+echo "\$V15=$V15"
+echo "\${#V15}=${#V15}"
+# ${#V15}=9
 
 # 部分文字列抽出
-echo "${V9:0:1},${V9:4}"
-# 1,56789
+echo "\${V15:0:2}, \${V15:4}=${V15:0:2}, ${V15:4}"
+# ${V15:0:2}, ${V15:4}=12, 56789
 
 # パターンに一致する文字列を置換(最短一致)して返す
-V10='a:b:c'
-V11='/var/log/messages'
-echo "${V10/:/#}"
-# a#b:c
-echo "${V11/\//\\}"
-# \var/log/messages
+V16='a:b:c'
+V17='/var/log/messages'
+echo "\$V16=$V16"
+echo "\$V17=$V17"
+echo "\${V16/:/#}=${V16/:/#}"
+echo "\${V17/\//\\}=${V17/\//\\}"
+# ${V16/:/#}=a#b:c
+# ${V17/\//\}=\var/log/messages
 
 # パターンに一致する文字列を置換(最長一致)して返す
-echo "${V10//:/#}"
-# a#b#c
-echo "${V11//\//\\}"
-# \var\log\messages
+echo "\${V16//:/#}=${V16//:/#}"
+echo "\${V17//\//\\}=${V17//\//\\}"
+# ${V16//:/#}=a#b#c
+# ${V17//\//\\}=\var\log\messages
 
 # 先頭からパターンに最短一致した部分までを取り除いて返す
-echo "${V11#*/}"
-# var/log/messages
+echo "\${V17#*/}=${V17#*/}"
+# ${V17#*/}=var/log/messages
 
 # 先頭からパターンに最長一致した部分までを取り除いて返す(ファイル名抽出)
-echo "${V11##*/}"
-# messages
+echo "\${V17##*/}=${V17##*/}"
+# ${V17##*/}=messages
 
 # 末尾からパターンに最短一致した部分までを取り除いて返す(ディレクトリ名抽出)
-echo "${V11%/*}"
-# /var/log
+echo "\${V17%/*}=${V17%/*}"
+# ${V17%/*}=/var/log
 
 # 末尾からパターンに最長一致した部分までを取り除いて返す
-echo "${V11%%/*}"
-# (空)
+echo "\${V17%%/*}=${V17%%/*}"
+# ${V17%%/*}=
 
 # LINENOは予約変数なので注意
-echo "LineNo=$LINENO"
+echo "\$LINENO=$LINENO"
 
 # ブレース展開
 echo /var/log/messages{,.1,.2}
 # /var/log/messages /var/log/messages.1 /var/log/messages.2
 
 # 配列展開
-V12=(A B 'C D' E)
+V18=(A B 'C D' E)
 
 # 配列の特定要素を取得
-echo "${V12[2]}"
-# C D
+echo "\${V18[2]}=${V18[2]}"
+# ${V18[2]}=C D
 
 # 配列の要素数を返す
-echo "${#V12[@]}"
-# 4
+echo "\${#V18[@]}=${#V18[@]}"
+# ${#V18[@]}=4
 
 # CSVを簡易配列展開(フィールドセパレータを変更)
-V13='a,b,c,d,e'
-_IFS=$IFS
+V19='a,b,c,d,e'
+_IFS="$IFS"
 IFS=,
-CSV=($V13)
+CSV=($V19)
 IFS="$_IFS"
-echo "${CSV[2]}"
-echo "${#CSV[*]}"
-# c
-# 5
+echo "\${CSV[2]}=${CSV[2]}"
+echo "\${#CSV[*]}=${#CSV[*]}"
+# ${CSV[2]}=c
+# ${#CSV[*]}=5
 
 # ヒアドキュメント
 cat <<__EOD__
 ----1
-$V9
+$V15
 `date +%Y%m%d`
 __EOD__
 
 # ヒアドキュメント(変数展開なし)
 cat <<'__EOD__'
 ----2
-$V9
+$V15
 `date +%Y%m%d`
 __EOD__
 
 # ヒアドキュメント(先頭タブ無視)
 cat <<-__EOD__
 	----3
-	$V9
-	`date +%Y%m%d`
-	  $V10
+	$V15
+	`date +%Y%m%d%H%M%S`
+	  indent
 __EOD__
 
 # 行頭の#でブロックコメントの切替(ヒアドキュメントの応用)
 #: <<'#__COMMENT__'
-V14=bar
+V20=bar
 #__COMMENT__
-echo "\$V14=${V14-default}"
+echo "\$V20=$V20"
 
 # クォート効果の違い(複数行記述)
-V15='Hello, 
+V21='Hello, 
 world!'
-echo $V15
-echo "$V15"
-V16="Hello, \
+echo $V21
+echo "$V21"
+V22="Hello, \
 world!"
-echo $V16
-echo "$V16"
+echo $V22
+echo "$V22"
+# Hello, world!
+# Hello,
+# world!
+# Hello, world!
+# Hello, world!
+
+# 位置パラメータの展開
+set -- sample1 'sample 2' sample3
+for V23 in "$*"; do
+	echo $V23
+done
+# sample1 sample 2 sample 3
+for V23 in "$@"; do
+	echo $V23
+done
+# sample1
+# sample 2
+# sample3
+
+# 受け取った引数をそのまま別プログラムや関数に渡す場合、 ${@+"$@"}を使うことで
+# 引数が指定されないのと空文字列が指定されたのを区別できる
+set --
+set -- ${@+"$@"}
+echo "\$#=$#"
+# $#=0
+set -- ""
+set -- ${@+"$@"}
+echo "\$#=$#"
+# $#=1
+
+# 標準出力をファイルに出力しつつ、標準エラー出力を変数に取得する
+TEMP=`mktemp -t ${0##*/}.XXXXXXXXXX`
+trap 'rm -f $TEMP' 0 1 2 3 15
+V24=`expr 1 / 0 3>&1 >$TEMP 2>&3`
+echo "Error: $V24"
